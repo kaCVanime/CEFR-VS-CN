1c6a5f6e085bea58c7f4ba6f0997a4207847c212
kaCVanime <kacvanime@gmail.com>
2024-08-04 16:42:48 +0800

    refactor

--- README.md
+++ README.md
@@ -1,58 +1,30 @@
-##
-此项目目的为解答以下问题
-> 国内各级英语应试，能否按照国外的英语水平标准(CEFR)来逐级学习单词
-
-**省流**：不能，重合度不大
-
-
-## venn
-
-**小学 VS CEFR A1**  
-![小学_A1](results/小学_A1.png)
-**小学 VS CEFR A2**  
-![小学_A2](results/小学_A2.png)
-**小学 VS CEFR B2**  
-![小学_B2](results/小学_B2.png)
-
----
-
-**初中 VS CEFR A2**  
-![初中_A2](results/初中_A2.png)
-**初中 VS CEFR B1**  
-![初中_B1](results/初中_B1.png)
-**初中 VS CEFR B2**  
-![初中_B2](results/初中_B2.png)
-
----
-
-**高中 VS CEFR B1**  
-![高中_B1](results/高中_B1.png)
-
----
-
-**四级 VS CEFR B1**  
-![四级_B1](results/四级_B1.png)
-**四级 VS CEFR B2**  
-![四级_B2](results/四级_B2.png)
-
----
-
-**六级 VS CEFR B2**  
-![六级_B2](results/六级_B2.png)
+### 国内各等级英语考试与国外的CEFR的关系
 
 --- 
-**专四专八 VS CEFR C1_C2**  
-![专四专八_C1C2](results/专四专八_C1C2.png)
+
+### 国内各考试的词汇在国外各等级中的分布
+> 想学国内某一等级(例如四级)，应该学到国外哪一等级？  
+
+![overview](results/overview.png)
+> _实现：为国内的各等级，顺着国外等级(由低到高)依次找交集，每次删减。都没找到则放进ULTRA_
 
 
+---
 
-### 项目结构
-**/origin**  
-[此项目](https://github.com/mahavivo/english-wordlists)整理的各词表  
-并修正几处微小错误  
-**/assets**  
-从原始词表中提取词头，并做去重处理使得各级间不重复  
-**/scripts**  
-脚本
+### 国外各等级之间相比，对国内各等级词汇的覆盖情况
+> 单学国外某等级，相当于国内综合实力为？
 
-_CEFR数据来源于牛津10，一个词条下面各释义的CEFR级别，各级间不做去重_
+![overview-keep](results/overview-keep.png)
+> _实现：为国内的各等级，顺着在国外等级(由低到高)依次找交集，不删减_
+
+---
+
+#### 目录结构
+**origin**  
+[此项目](https://github.com/mahavivo/english-wordlists)中的原始cn词表  
+**assets**
+1. 从各原始cn词表提取词头，并相互删减，使得没有交集  
+2. COCA_20000。去重的COCA20000词频  
+3. CEFR A1-C2。 以COCA20000为基础在牛津10辞典中提取  
+4. ULTRA。国内要求但国外不要求的词。大部分为专四专八，少量极其普遍的词或变形词
+
--- scripts/main.py
+++ scripts/main.py
@@ -1,49 +1,49 @@
 from pathlib import Path
-from recorder import Recorder
 
-import matplotlib.pyplot as plt
-from matplotlib_venn import venn2
-
-plt.rcParams['font.sans-serif'] = ['SimHei']
-
-db = Recorder()
+assets_path = Path.cwd().parent / 'assets'
 
 
-def get_word_list_set(fp):
-    with open(fp, mode='r', encoding='utf-8') as f:
-        lines = [s.strip() for s in f.readlines()]
+def get_word_list_set(name):
+    with open(assets_path / (name + '.txt'), mode='r', encoding='utf-8') as f:
+        lines = [s.strip().lower() for s in f.readlines()]
 
     return set([l for l in lines if l])
 
-def show_venn2(s1, s2, s1_name, s2_name):
-    intersection = s1.intersection(s2)
-    only_s1 = s1 - intersection
-    only_s2 = s2 - intersection
-    venn2(subsets=(len(only_s1), len(only_s2), len(intersection)), set_labels=(s1_name, s2_name))
-    plt.show()
-
-
-def show_analysis(wn, cn, wl, cl, cn2=None):
-    assets_path = Path.cwd() / 'assets'
-    a = get_word_list_set(assets_path / f'{wn}.txt')
-    b = set(e['word'] for e in db.getCEFR(cn, cn2))
-
-    show_venn2(a, b, wl, cl)
 
 def main():
-    # show_analysis('小学', 'A1', '小学词汇', 'CEFR_A1')
-    # show_analysis('初中', 'A2', '初中词汇', 'CEFR_A2')
-    # show_analysis('高中', 'B1', '高中词汇', 'CEFR_B1')
-    # show_analysis('四级', 'B1', '四级词汇', 'CEFR_B1')
-    # show_analysis('六级', 'B2', '六级词汇', 'CEFR_B2')
-    # show_analysis('小学', 'B2', '小学词汇', 'CEFR_B2')
-    show_analysis('专四专八', 'B2', '专四专八词汇', 'CEFR_C1_C2', 'C2')
 
+    sources = [(name, get_word_list_set(name)) for name in ['小学', '初中', '高中', '四级', '六级', '专四专八']]
+
+    targets = [(name, get_word_list_set(name)) for name in ['CEFR_A1', 'CEFR_A2', 'CEFR_B1', 'CEFR_B2', 'CEFR_C1', 'CEFR_C2', 'COCA_20000']]
+
+    # targets.append(('ULTRA', set()))
+
+    nodes = [{"name": name, "count": len(wl)} for name, wl in [*sources, *targets]]
+
+    links = []
+    for src, swl in sources:
+        for tgt, twl in targets:
+            intersection = swl.intersection(twl) if tgt != 'ULTRA' else swl
+            if intersection:
+                links.append({ "source": src, "target": tgt, "value": len(intersection)})
+                # links.append({ "source": src, "target": tgt, "value": len(intersection), "i": intersection if tgt == 'ULTRA' else None})
+                # swl = swl - intersection
+
+    # ultra = []
+    # for l in links:
+    #     if l["i"]:
+    #         ultra.extend(list(l["i"]))
+    # ultra.sort()
+    # with open(assets_path / ('ULTRA' + '.txt'), encoding='utf-8', mode='w') as f:
+    #     f.writelines([(w + '\n') for w in ultra])
+
+    graph = {
+        "nodes": nodes,
+        "links": links
+    }
     pass
 
 
 
 if __name__ == '__main__':
-    db.start()
     main()
-    db.close()
